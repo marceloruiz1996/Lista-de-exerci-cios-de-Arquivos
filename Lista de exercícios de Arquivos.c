bee02de2b3ca7beaf3bc5e7c4ef08a8cdb4818a1
marcelo ruiz <marcelo.silva19@estudante.ifms.edu.br>
2025-04-29 15:51:55 -0300

    Update Lista de exercícios de Arquivos.c

--- Lista de exercícios de Arquivos.c
+++ Lista de exercícios de Arquivos.c
@@ -10,6 +10,62 @@ typedef struct
   char Fone[20];
   } Cliente;
 
+#difine TAM 1000000; // tamanho 
+clientes[TAM];
+int quant=0;
+
+int cadastrarCliente(Cliente *c)
+  {
+  if(quant<TAM)
+    {
+    printf("Cadastro de cliente\n");
+    printf("código do Cliente: ");
+    scanf("%d", & c->.Cod_Cli);
+    getchar(); //limpa o \n (buffer)do scanf para nao bugar os fgets;
+    printf("\nNome: ");
+    fgets(c->Nome, 50, stdin);
+    printf("\nEndereco: ");
+    fgets(c->Endereco, 400, stdin);
+    printf("\nFone: ");
+    fgets(c->Fone, 20, stdin);
+    quant++;
+    return 0;
+    }
+  else
+    {
+    printf("erro, quantidade máxima de cadastro efetuada");
+    }
+  }
+
+void listarClientes()
+  {
+  int i;
+  for(i=0, i < quant, i++)
+    {
+    printf("cliente %d\n", i+1);
+    printf("código: %d\n", clientes[i].Cod_Cli);
+    printf("nome: %c\n", clientes[i].Nome);
+    printf("endereço: %c\n", clientes[i].Endereco);
+    printf("telefone: %c\n", clientes[i].Fone);    
+    }
+  }
+
+void arquivoCliente()
+  {
+  FILE *arq = fopen("cliente.txt", "w");
+  if(arq)
+    {
+    for(i=0, i, i++)
+      {
+      
+      }
+    }
+  else
+    {
+    printf("ERRO");
+    }
+  }
+
 typedef struct
   {
   int Num_doc;
@@ -19,22 +75,6 @@ typedef struct
   int Cod_Cli;
   } Recebimento;
 
-int cadastrarCliente(Cliente *c)
-  {
-  printf("Cadastro de cliente\n");
-  printf("código do Cliente: ");
-  scanf("%d", & c->.Cod_Cli);
-  getchar(); //limpa o \n (buffer)do scanf para nao bugar os fgets;
-  printf("\nNome: ");
-  fgets(c->Nome, 50, stdin);
-  printf("\nEndereco: ");
-  fgets(c->Endereco, 400, stdin);
-  printf("\nFone: ");
-  fgets(c->Fone, 20, stdin);
-
-  return 0;
-  }
- 
 int cadastrarRecebimento(Recebimento *r)
   {
   printf("Cadastro de recebimento\n");
@@ -53,26 +93,35 @@ int cadastrarRecebimento(Recebimento *r)
   
   return 0;
   }
-
-void salvarArquivo()
-  {
-  FILE *arq = fopen("cliente.txt", "w");
-  if(arq)
-    { }
-  else
-    {
-    printf("ERRO");
-    }
-  
-  }
-
+int 
 int main()
   {
-  Cliente cliente;
-  Recebimento recebimento;
-
-  cadastrarCliente(&cliente);
-  cadastrarRecebimento(&recebimento);
+  int op;
   
+  do
+    {
+    printf("\n1 - cadastrar cliente \n2 - imprimir cliente\n0 - sair");
+    scanf("%d", & op);
+    
+    switch(op)
+      {
+      case 0:
+        prinft("encerrado");
+        break;
+      
+      case 1:
+        cadastrarCliente();
+        break;
+
+      case 2:
+        listarClientes();
+        break;
+      
+      default:
+        printf("opção invalida");
+      }while(op != 0);    
+    }
+  
+ 
   return 0;
   }
